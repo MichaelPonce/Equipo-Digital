bdb6ca5316a37538f3338003a54c20f86a550f50
MichaelPonce <66215641+MichaelPonce@users.noreply.github.com>
2020-07-31 21:46:32 -0600

    Update mail-sender.php

--- mail-sender.php
+++ mail-sender.php
@@ -1,10 +1,11 @@
 <?php
 
-  $destino= "Maycolponce21@gmail.com"
-  $nombre= $_POST["nombre"]
-  $email= $_POST["email"]
-  $mensaje= $_POST["mensaje"]
+  $destino= "Maycolponce21@gmail.com";
+  $Nombre= $_POST["nombre"];
+  $Email= $_POST["email"];
+  $Mensaje= $_POST["mensaje"];
 
-  $contenido = "Nombre: " . $nombre . "\nCorreo: ". $email . "\nMensaje: ". $mensaje;
+  $contenido = "Nombre: " . $Nombre . "\nCorreo: ". $Email . "\nMensaje: ". $Mensaje;
   mail ($destino, "Contacto", $contenido);
+  header("Location:index-1.html");
  ?>
